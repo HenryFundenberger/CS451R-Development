f3e4debd09ca642934bb9dad0ccb9dab67067920
HenryFundenberger <63514033+HenryFundenberger@users.noreply.github.com>
2023-02-22 20:32:38 -0600

    Add files via upload

---
+++ spring-boot-turial/src/test/java/com/example/springbootturial/SpringBootTurialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootturial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTurialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
